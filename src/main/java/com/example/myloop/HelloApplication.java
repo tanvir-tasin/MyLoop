e2d3851bf15706be88c23422a9fe3505f0717808
MD Tanvirul Islam Tasin <tanvirtasinofficial@gmail.com>
2024-03-20 01:37:33 +0600

    let's Start

--- src/main/java/com/example/myloop/HelloApplication.java
+++ src/main/java/com/example/myloop/HelloApplication.java
@@ -3,7 +3,10 @@ package com.example.myloop;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -11,9 +14,13 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        scene.setFill(Color.TRANSPARENT);
+        // scene.getStylesheets().add(getClass().getResource("stylish.css").toExternalForm());
+
+        stage.getIcons().add(new Image("icon.png"));
         stage.setScene(scene);
+        stage.initStyle(StageStyle.TRANSPARENT);
         stage.show();
     }
 
--- src/main/java/com/example/myloop/HelloController.java
+++ src/main/java/com/example/myloop/HelloController.java
@@ -1,14 +1,113 @@
 package com.example.myloop;
 
+
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.util.Duration;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class HelloController implements Initializable {/*
+@FXML
+    TextField lName;
+@FXML
+TextField lpass;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    TextField NewName;
+    @FXML
+    TextField Newpass1;
+    @FXML
+    TextField Newemail;
+    @FXML
+    TextField Newpass2;
+*/    @FXML
+    private VBox vbox=new VBox();
+@FXML
+Text text1;
+@FXML
+Text text2;
+    @FXML
+    private Parent fxml;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        //SwingNode swingNode=new SwingNode();
+        TranslateTransition t=new TranslateTransition(Duration.seconds(1),vbox);
+        t.setToX(vbox.getLayoutX()*15);
+        t.play();
+        t.setOnFinished((e)->{
+                    try {
+                        fxml= FXMLLoader.load(getClass().getResource("Login.fxml"));
+                        vbox.getChildren().removeAll();
+                        vbox.getChildren().setAll(fxml);
+
+                    }
+                    catch (IOException ex)
+                    {
+                        ex.printStackTrace();
+                    }
+                }
+        );
     }
+    @FXML
+    private void opensignin(ActionEvent event)
+    {
+        TranslateTransition t=new TranslateTransition(Duration.seconds(1),vbox);
+        t.setToX(vbox.getLayoutX()*15);
+        t.play();
+        t.setOnFinished((e)->{
+                    try {
+
+                        fxml= FXMLLoader.load(getClass().getResource("Login.fxml"));
+                        vbox.getChildren().removeAll();
+                        vbox.getChildren().setAll(fxml);
+
+                    }
+                    catch (IOException ex)
+                    {
+                        ex.printStackTrace();
+                    }
+                }
+        );
+
+    }
+    @FXML
+    private void opensignup(ActionEvent event)
+    {
+        TranslateTransition t=new TranslateTransition(Duration.seconds(1),vbox);
+        t.setToX(0);
+        t.play();
+        t.setOnFinished((e)->{
+                    try {
+
+                        fxml= FXMLLoader.load(getClass().getResource("CreateAccount.fxml"));
+                        vbox.getChildren().removeAll();
+                        vbox.getChildren().setAll(fxml);
+
+                    }
+                    catch (IOException ex)
+                    {
+                        ex.printStackTrace();
+                    }
+                }
+        );
+
+    }
+    /*String newname=NewName.getText();
+    String newemail=Newemail.getText();
+    String newPass1=Newpass1.getText();
+    String newPass2=Newpass2.getText();
+    String lname=lName.getText();
+    String lpass1=lpass.getText();*/
+
 }
